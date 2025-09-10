8aa006311546274318a97b1b0d2c61ded0ed3b52
Priyank Agarwal <paga@kth.se>
2025-09-10 08:30:02 +0200

    Update bin2bcd.sv
    q2_working code

--- q2/bin2bcd.sv
+++ q2/bin2bcd.sv
@@ -1,9 +1,28 @@
-module bin2bcd (
-  input logic [3:0] binary,
-  output logic [3:0] bcd,
-  output logic carry
+
+module hex_to_bcd(
+    input logic [3:0] hex_in,
+    output logic [3:0] BCD,
+    output logic carry
 );
-  // …
-  // Add your description here
-  // …
+
+logic [15:0] bcd1, bcd2,bcd3,bcd4,carry_lut;
+
+assign bcd4 = 16'b0000001100000000;
+assign bcd3 = 16'b1100000011110000;
+assign bcd2 = 16'b0011000011001100;
+assign bcd1 = 16'b1010101010101010;
+assign carry_lut = 16'b11111110000000000;
+
+always_comb begin
+    BCD = 4'b0;
+    carry = 1'b0;
+
+    BCD[0] = bcd1[hex_in];
+    BCD[1] = bcd2[hex_in];
+    BCD[2] = bcd3[hex_in];
+    BCD[3] = bcd4[hex_in];
+    carry = carry_lut[hex_in];
+
+end
+
 endmodule
