1202e13024ac3ec58f4aab7fd1dccb3d833ff17c
Priyank Agarwal <paga@kth.se>
2025-09-10 08:39:07 +0200

    Update decoder_tb.sv
    test bench

--- q1/decoder_tb.sv
+++ q1/decoder_tb.sv
@@ -1,14 +1,21 @@
-module decoder_tb;
+module q1_tb;
+    logic [3:0] de_mux_sel;
+    logic [15:0] one_hot_out;
 
-  // Testbench signals
-  logic [3:0] binary;
-  logic [15:0] one_hot;
 
-  // Instantiate the decoder module
-  decoder uut (
-    .binary(binary),
-    .one_hot(one_hot)
-  );
 
-  // Complete your testbench here
+one_hot_ecoder DUT(
+    .binary(de_mux_sel),
+    .one_hot(one_hot_out)
+);
+
+initial begin
+    for(int i=0;i<16;i++) begin
+        de_mux_sel = i;
+        #10ns;
+    end
+    
+end
+
 endmodule
+
